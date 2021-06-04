81236b1affe71f0e2bbaa54110a1613716596c1d
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-06-04 16:27:19 -0400

    basic client/server

---
+++ server/server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var favicon = require('serve-favicon');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use(favicon(path.join(__dirname, '..', 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
+app.post('/hello', function(req, res) {
+  res.json('hello');
+});
+
+app.use(function(err, req, res, next) {
+  res.json('error');
+});
+
+module.exports = app;
